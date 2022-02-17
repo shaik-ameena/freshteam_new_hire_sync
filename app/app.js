5fa9627a84e8a7ae25aac5dca44ca908115e484e
Ameena Shaik <n150667@rguktn.ac.in>
2022-02-17 20:51:27 +0530

    skeletion for new hire sync is completed

--- app/app.js
+++ app/app.js
@@ -2,16 +2,17 @@ $(document).ready( function() {
     app.initialized()
         .then(function(_client) {
           window.client = _client;
-          console.log("client at beg" , client);
+          //console.log("client at beg" , client);
           onAppInitialise();//for this full page app app.activated is not getting called
     });
 });
 function onAppInitialise(){
-    console.log("for every time when app is initialised");
-    document.getElementById("role").addEventListener("fwChange",autoFillTemplate);
-    displayAllTemplates();
-
+     getAllDesignations();
+     document.getElementById("role").addEventListener("fwChange",autoFillTemplate);
+     displayAllTemplates();
+    
 }
+
 function onSave(){
     //let selected_roles_list = $("#role-list").value;
     let selected_role = document.getElementById("role").value;
@@ -54,7 +55,7 @@ function displayAllTemplates(){
     for(let i=0;i<roles.length ; i++){
         client.db.get(roles[i]).then (
             function(data) {
-                console.log("retrived data ", data  , data.subject);
+                //console.log("retrived data ", data  , data.subject);
                 content += `<tr>
                 <td><div onclick="redirectToEdit()" id="template-name">${roles[i]}</div></td>
                 <td><div>${data.subject}</div></td>
@@ -72,4 +73,34 @@ function redirectToEdit(){
     console.log("redirect to edit is caleed");
     let tabsParent = document.getElementById("tabs-root");
     console.log("active tab ", tabsParent.active-tab-index);
+}
+
+function getAllDesignations(){
+    let selectContainer = document.getElementById('roles');
+    const options = {
+        headers :{
+            Authorization:"Bearer <%= iparam.freshteam_api_key %>",
+            accept: "application/json",
+        }
+    }
+    let content = `<fw-select  placeholder="Your choices">` ;
+    client.request.get("https://ameenashaik.freshteam.com/api/roles" , options).then(function(data) {
+        if(data.status == 200){
+            const response = JSON.parse(data.response);
+            const len = response.length;
+            console.log("response ",response ,len);
+            for(let i=0;i<response.length ; i++){
+                console.log("at i " , i , " and " , response[i].name);
+                content += `<fw-select-option value="${response[i].name}">${response[i].name}</fw-select-option>`;
+            }
+            content += `</fw-select>`;
+            console.log("final content is ", content);
+            selectContainer.innerHTML = content;
+        }
+     },
+     function(error) {
+       console.log("failed to get roles ", roles);
+     });
+    //selectContainer.innerHTML = content;
+   
 }
