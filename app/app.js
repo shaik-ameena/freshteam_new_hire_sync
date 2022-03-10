03a8903d35429c47f3ce49f682dbb221fbff85fe
Ameena Shaik <n150667@rguktn.ac.in>
2022-03-10 13:45:10 +0530

    new hire kick start sample app completed

--- app/app.js
+++ app/app.js
@@ -1,106 +1,189 @@
+
 $(document).ready( function() {
     app.initialized()
-        .then(function(_client) {
-          window.client = _client;
-          //console.log("client at beg" , client);
-          onAppInitialise();//for this full page app app.activated is not getting called
+        .then(function(client) {
+          window.client = client;
+          console.log("client object is ", client);
+          onAppInitialise();
+
     });
 });
 function onAppInitialise(){
      getAllDesignations();
-     document.getElementById("role").addEventListener("fwChange",autoFillTemplate);
      displayAllTemplates();
-    
+     displayAllSentMails();
+     displayAllFailedMails();
+     autoFillTemplate(true);
 }
 
-function onSave(){
-    //let selected_roles_list = $("#role-list").value;
+function onTemplateSave(){
     let selected_role = document.getElementById("role").value;
     let template_sub = document.getElementById("template-subject").value;
     let template_body = document.getElementById("template-body").value;
-   
-    client.db.set(selected_role , {"subject":template_sub,"body":template_body}).then (
+    client.db.get("allSavedRolesTemplates").then (
         function(data) {
-            console.log("templated saved successfully", data);
+          let savedOnes = data.savedRolesTemplate;
+          const dupEntry = (savedOne) => savedOne.role === selected_role;
+          let dupEntryIndex = savedOnes.findIndex(dupEntry);
+          if(dupEntryIndex == -1 && selected_role !== '#'){
+            savedOnes.push({"role":selected_role, "subject":template_sub,"body":template_body});
+          }
+          else{
+              savedOnes[dupEntryIndex].subject = template_sub;
+              savedOnes[dupEntryIndex].body = template_body;
+          }
+          client.db.set("allSavedRolesTemplates", {"savedRolesTemplate" : savedOnes}).then (
+            function(data) {
+               console.log("successfully updated ", data );
+               onAppInitialise();
+            },
+            function(error) {
+              console.log("updation failed ",error)
+            });
+        
         },
         function(error) {
-          console.log(" failed to save template", error)
-        });
-    //for intialising app again
-    onAppInitialise();
+          console.log("failed to get templates  ", error);
+          client.db.set( "allSavedRolesTemplates",{"savedRolesTemplate":[]}).then (
+            function(data) { console.log("templates data store initialised",data);
+            },function(error) {console.log("failed to initialise  " , error);
+            });
+    });
 }
-function autoFillTemplate(){
+
+function autoFillTemplate(manualCall){
+    console.log("auto fill template ", manualCall);
     let selected_role = document.getElementById("role").value;
     let template_sub = document.getElementById("template-subject");
     let template_body = document.getElementById("template-body");
-    client.db.get(selected_role).then (
+    if(!manualCall){
+    client.db.get("allSavedRolesTemplates").then (
         function(data) {
-            template_sub.value = data.subject;
-            template_body.value = data.body;
+            let {savedRolesTemplate} = data;
+            let matchedTemplate = savedRolesTemplate.filter(savedOne=>savedOne.role === selected_role);
+            if(matchedTemplate.length != 0){
+                template_sub.value = matchedTemplate[0].subject;
+                template_body.value = matchedTemplate[0].body;
+            }
+            else{
+                template_sub.value = "";
+                template_body.value = "";
+            }      
         },
         function(error) {
+            console.log("error while getting templates", error);
+        });
+    }
+    else{
+        setTimeout(() => {
             template_sub.value = "";
             template_body.value = "";
-        });
-}
-
-function displayAllTemplates(){
-    const roles = ["Software Engineer","Hiring Manager","Account Admin","Recruiter"];
-    let temp = document.getElementById("saved-templates");
-    let content = `<table id="templates">
-      <tr>
-        <th>Role</th>
-        <th>Template Subject</th>
-     </tr>`;
-    for(let i=0;i<roles.length ; i++){
-        client.db.get(roles[i]).then (
-            function(data) {
-                //console.log("retrived data ", data  , data.subject);
-                content += `<tr>
-                <td><div onclick="redirectToEdit()" id="template-name">${roles[i]}</div></td>
-                <td><div>${data.subject}</div></td>
-                </tr>`;
-                temp.innerHTML = content;
-            },
-            function(error) {
-                console.log("erro" , error);
-              // failure operation
-            });
+        }, 1000);
     }
 }
 
-function redirectToEdit(){
-    console.log("redirect to edit is caleed");
-    let tabsParent = document.getElementById("tabs-root");
-    console.log("active tab ", tabsParent.active-tab-index);
-}
-
 function getAllDesignations(){
-    let selectContainer = document.getElementById('roles');
+    let selectContainer = document.getElementById('designation');
     const options = {
         headers :{
             Authorization:"Bearer <%= iparam.freshteam_api_key %>",
             accept: "application/json",
         }
     }
-    let content = `<fw-select  placeholder="Your choices">` ;
-    client.request.get("https://ameenashaik.freshteam.com/api/roles" , options).then(function(data) {
+    let content = `<select name="cars" id="role" onChange = autoFillTemplate()><option value="#">-----------select role----------</option>` ;
+    client.request.get("https://ameenashaik.freshteam.com/api/job_postings" , options).then(function(data) {
         if(data.status == 200){
             const response = JSON.parse(data.response);
-            const len = response.length;
-            console.log("response ",response ,len);
             for(let i=0;i<response.length ; i++){
-                console.log("at i " , i , " and " , response[i].name);
-                content += `<fw-select-option value="${response[i].name}">${response[i].name}</fw-select-option>`;
+                content += `<option value="${response[i].title}">${response[i].title}</option>`;
             }
-            content += `</fw-select>`;
-            console.log("final content is ", content);
+            content += `</select>`;
             selectContainer.innerHTML = content;
         }
      },
      function(error) {
-       console.log("failed to get roles ", roles);
+       console.log("failed to get roles ", error);
      });
-    //selectContainer.innerHTML = content;
-   
 }
+function displayAllTemplates(){
+    client.db.get("allSavedRolesTemplates").then (
+        function( {savedRolesTemplate} ) {
+            let savedTemplatesTab = document.getElementById('saved-templates');
+            let content = generateDynamicTable(savedRolesTemplate,["Designation","Template Subject"],["role","subject"],["","",""],true,"role");
+            savedTemplatesTab.innerHTML = content;
+        },
+        function(error) {
+            console.log("failed to get templates ",error)
+        });
+}
+
+function displayAllFailedMails(){
+    client.db.get("allFailedMailLogs").then (
+        function( {failedMailLogs} ) {
+          let failedMailsTab = document.getElementById('failed-mails');
+          let content = generateDynamicTable(failedMailLogs,["New Hire Email","Role","Message","Date"],["email","role","status_msg","date"],["20%","15%","","15%"],false,"");
+          failedMailsTab.innerHTML = content;
+        },
+        function(error) {
+           console.log("error while fetching failed mail logs ",error);
+    });
+}
+function displayAllSentMails(){
+    client.db.get("allSentMailLogs").then (
+        function({sentMailLogs}) {
+          let sentMailsTab = document.getElementById('sent-mails');
+          let content = generateDynamicTable(sentMailLogs,["New Hire Email","Role","Message","Date"],["email","role","status_msg","date"],["20%","15%","","15%"],false,"");
+          sentMailsTab.innerHTML = content;
+        },
+        function(error) {
+            console.log("sent mails logs not found ",error);
+    });
+}
+function generateDynamicTable(data , titles , attributes ,widths, deleteOption,deleteKey){
+    let content = `<table id="templates">
+    <tr><th>#</th>`;
+    for(let i=0 ; i< titles.length ; i++){
+        content += `<th style="width:${widths[i]}">${titles[i]}</th>`;
+    }
+    if(deleteOption){
+        content += `<th style="width:7%"></th>`;
+    }
+    content += `</tr>`;
+    for(let j=0;j< data.length ; j++){
+        content += `<tr><td>${j+1}</td>`;
+        for(let k=0; k< attributes.length ; k++){
+            content += `<td><div>${data[j][attributes[k]]}</div></td>`       
+        }
+        if(deleteOption){
+            let roleToBeDeleted = data[j][deleteKey];
+            content += `<td><div><button value='${roleToBeDeleted}' onclick=onTemplateDelete(this)>Remove</button></div></td>`;
+            //content += `<i class="fa fa-remove" style="font-size:24px"></i>`;
+        }
+        content += `</tr>`;
+    }
+    content += `</table`;
+    return content;
+}
+function onTemplateDelete(e){
+    let deleteKey = e.value;
+    console.log("request raised for deleting template ", deleteKey);
+    client.db.get("allSavedRolesTemplates").then (
+        function( {savedRolesTemplate} ) {
+          const filteredRolesTemplates = savedRolesTemplate.filter(savedOne=>savedOne.role !== deleteKey);
+          client.db.set( "allSavedRolesTemplates", { "savedRolesTemplate": filteredRolesTemplates }).then (
+            function(data) {
+              console.log("successfully stored updated role templates ", data);
+              onAppInitialise();
+            },
+            function(error) {
+              console.log( " failed to store updated role templates",error)
+            });
+        },
+        function(error) {
+            console.log("error while fetchinh roles from store ",error);
+     });
+}
+
+
+
+
--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const nodemailer = require("nodemailer");
-exports = {
+
+exports = {  
     onAppInstallCallback:  function(payload) {
+        console.log("on app install called " , payload);
         const options = {
             headers :{
                 Authorization:"Bearer <%= iparam.freshteam_api_key %>",
@@ -12,45 +14,99 @@ exports = {
                renderData();
         },
         function(error) {
-           renderData({message:"Domain Validation failed!!!"});
-           
+           renderData({message:`Domain Validation failed!!! , ${error}`});  
         });
     },
-    onEmployeeCreateCallback: function(payload) {
+    onNewHireCreateCallback: function(payload) {
+        //console.log("new hire create called " , JSON.stringify(payload));
         let transporter = nodemailer.createTransport({
             host: 'smtp.gmail.com',
             port: 465,
             secure: true, // use SSL
             auth: { 
               user:payload.iparams.email, 
-              pass:payload.iparams.email_passw
-            },
+              pass:payload.iparams.email_passw,
+           },
         });
-        let roles = payload.data.associations.roles;
-        //employee may have multiple designations
-        for(let i=0; i<roles.length ; i++){
-          $db.get(roles[i].name).then (
-            function(data) {
-                console.log("retrived data ", data  , data.subject);
-                let options = {
-                  from: payload.iparams.email,
-                  to:payload.data.actor.email,
-                  //to:"ameena.shaik@freshworks.com",
-                  subject: data.subject,
-                  text: data.body,
-                  //html: "<b>Hello world?</b>",
-                };
-                transporter.sendMail(options, function(error, info){
-                  if (error) {
-                    console.log("error is " , error);
-                  } else {
-                    console.log('Email sent successfully : ' + info.response);
-                  }
-                });
-            },
-            function(error) {
-                console.log("role template no found" , error);
-            });
-        }  
+        let newHireDesignation = payload.data.newhire.designation;
+        $db.get("allSavedRolesTemplates").then (
+          function(data) {
+              let {savedRolesTemplate} = data;
+                const matchedRole = (savedRole) => savedRole.role === newHireDesignation;
+                let matchedRoleIndex = savedRolesTemplate.findIndex(matchedRole);
+                if(matchedRoleIndex != -1){
+                    let options = {
+                      from: payload.iparams.email,
+                      //to:payload.data.actor.email,
+                      to:payload.data.newhire.personal_email,
+                      subject: savedRolesTemplate[matchedRoleIndex].subject,
+                      text: savedRolesTemplate[matchedRoleIndex].body,
+                    };
+                    transporter.sendMail(options, function(error, info){
+                      if (!error) {
+                        console.log("email sent successfully " , info.response , options);
+                        $db.get("allSentMailLogs").then (
+                          function(data) {
+                          let mails = data.sentMailLogs;
+                          let today = new Date().toLocaleString();
+                          mails.push({ role:savedRolesTemplate[matchedRoleIndex].role ,email:payload.data.newhire.personal_email,status_msg:"Email sent successfully",  sent:true ,date:today});
+                          $db.set( "allSentMailLogs", {"sentMailLogs":mails}).then (
+                            function(data) { console.log("updated sent mails at db",data);},
+                            function(error) { console.log("failed to update sent mails at db " , error); });
+                        },
+                        function(error) {
+                          let today = new Date().toLocaleString();
+                          console.log("first sent mail error ", error , savedRolesTemplate[matchedRoleIndex].role);
+                          $db.set( "allSentMailLogs",{"sentMailLogs":[{ role:savedRolesTemplate[matchedRoleIndex].role ,email:payload.data.newhire.personal_email,status_msg:"Email sent successfully",  sent:true ,date:today}]}).then (
+                            function(data) { console.log("sent mails db initialised",data); },
+                            function(error) { console.log("failed to initalise sent mails db" , error); })
+                        });
+                      }
+                      else{
+                        console.log("error is failed to send mail ", error.response);
+                        $db.get("allFailedMailLogs").then (
+                          function(data) {
+                          let mails = data.failedMailLogs;
+                          let today = new Date().toLocaleString();
+                          mails.push({ role:savedRolesTemplate[matchedRoleIndex].role ,email:payload.data.newhire.personal_email,status_msg:error.response,  sent:false ,date:today});
+                          $db.set( "allFailedMailLogs", {"failedMailLogs":mails}).then (
+                            function(data) { console.log("updated failed mails at db",data);},
+                            function(error) { console.log("failed to update failed mails at db " , error); });
+                        },
+                        function(err) {
+                          console.log("error is ",err);
+                          let today = new Date().toLocaleString();
+                          $db.set( "allFailedMailLogs",{"failedMailLogs":[{role:newHireDesignation ,email:payload.data.newhire.personal_email,status_msg:error.response,  sent:false ,date:today}]}).then (
+                            function(data) { console.log("failed mails db initialised",data); },
+                            function(error) { console.log("failed to initalise failed mails db" , error); })
+                        });
+                      }
+                    })
+                }
+                else{
+                  //template not found case
+                  $db.get("allFailedMailLogs").then (
+                    function(data) {
+                    let mails = data.failedMailLogs;
+                    let today = new Date().toLocaleString();
+                    mails.push({ role:newHireDesignation ,email:payload.data.newhire.personal_email,status_msg:"Template not found",  sent:false ,date:today});
+                    $db.set( "allFailedMailLogs", {"failedMailLogs":mails}).then (
+                      function(data) { console.log("no template found at db",data);},
+                      function(error) { console.log("failed to update failed mails at db at template nt found " , error); });
+                  },
+                  function(error) {
+                    let today = new Date().toLocaleString();
+                    console.log("first failed mails ", error);
+                    $db.set( "allFailedMailLogs",{"failedMailLogs":[{ role:newHireDesignation  ,email:payload.data.newhire.personal_email,status_msg:"Template not found",  sent:false ,date:today}]}).then (
+                      function(data) { console.log("failed mails db initialised",data); },
+                      function(error) { console.log("failed to initalise failed mails db" , error); })
+                  });
+                }
+              //}
+          },
+          function(error) {
+              console.log("failed to get saved templates ", error);
+          });
+       
     }
 };
